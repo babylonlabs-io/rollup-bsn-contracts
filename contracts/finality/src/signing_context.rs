cb25cd848ab40516601ee278612f3fc176035e8c
Runchao Han <me@runchao.rocks>
2025-07-08 14:53:07 +1000

    init signing ctx lib

--- contracts/finality/src/signing_context.rs
+++ contracts/finality/src/signing_context.rs
@@ -1,4 +1,8 @@
+use babylon_bindings::BabylonQuery;
+use cosmwasm_std::{Deps, DepsMut, Env, StdResult};
 use k256::sha2::{Digest, Sha256};
+
+use crate::state::config::get_config;
 
 const PROTOCOL_NAME: &str = "btcstaking";
 const VERSION_V0: &str = "0";
@@ -14,19 +18,31 @@ fn btc_staking_v0_context(operation_tag: &str, chain_id: &str, address: &str) ->
 
 /// Returns the hex encoded sha256 hash of the context string i.e
 /// hex(sha256(context_string))
-pub fn hashed_hex_context(context_string: &str) -> String {
+fn hashed_hex_context(context_string: &str) -> String {
     let hash = Sha256::digest(context_string.as_bytes());
     hex::encode(hash)
 }
 
 /// Returns context string in format:
 /// btcstaking/0/fp_rand_commit/{chain_id}/{address}
-pub fn fp_rand_commit_context_v0(chain_id: &str, address: &str) -> String {
+fn fp_rand_commit_context_v0(chain_id: &str, address: &str) -> String {
     hashed_hex_context(&btc_staking_v0_context(FP_RAND_COMMIT, chain_id, address))
 }
 
 /// Returns context string in format:
 /// btcstaking/0/fp_fin_vote/{chain_id}/{address}
-pub fn fp_fin_vote_context_v0(chain_id: &str, address: &str) -> String {
+fn fp_fin_vote_context_v0(chain_id: &str, address: &str) -> String {
     hashed_hex_context(&btc_staking_v0_context(FP_FIN_VOTE, chain_id, address))
+}
+
+pub fn get_fp_rand_commit_context_v0(deps: Deps<BabylonQuery>, env: Env) -> StdResult<String> {
+    let bsn_id = get_config(deps)?.bsn_id;
+    let address = env.contract.address.to_string();
+    Ok(fp_rand_commit_context_v0(&bsn_id, &address))
+}
+
+pub fn get_fp_fin_vote_context_v0(deps: Deps<BabylonQuery>, env: Env) -> StdResult<String> {
+    let bsn_id = get_config(deps)?.bsn_id;
+    let address = env.contract.address.to_string();
+    Ok(fp_fin_vote_context_v0(&bsn_id, &address))
 }
